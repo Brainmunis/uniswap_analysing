d013162a0e6008836b88d18709b8d398f374bc37
Brian <brian.munis-at-339675176154>
2022-05-17 14:15:30 +0530

    block refresh if it is already running

--- pages/api/pools/refresh/[poolId].ts
+++ pages/api/pools/refresh/[poolId].ts
@@ -6,7 +6,8 @@ import startEventProcessing from '../../../../migrations/pools'
 export default async (req :NextApiRequest, res : NextApiResponse) => {
     const {
         poolId,
-        fromBlock
+        fromBlock,
+        force
     } = req.body;
     
     if(!req.body.hasOwnProperty('fromBlock')){
@@ -24,6 +25,19 @@ export default async (req :NextApiRequest, res : NextApiResponse) => {
     }
     if(!exists.rows.length){
         return res.status(400).send({ error : "Invalid pool id"})
+    }
+    const indexTrackerQuery = `SELECT * FROM indexing_tracker WHERE pool_id = ${poolId} AND event_type like 'Mint'`
+    const [terr, indexTracker] = await wait(
+        conn.query,
+        conn,
+        indexTrackerQuery
+    )
+    if(terr){
+        return res.status(500).send({ error : JSON.stringify(terr)})
+    }
+    if(indexTracker && indexTracker.rows && 
+        indexTracker.rows.length && indexTracker.rows[0].index_status === "initiated" && !force){
+            return res.status(400).send("Refreshing for this pool already running. Provide force=true option inside body to forcfully refresh this pool.")
     }
     const [peerr, data] = await wait(
         startEventProcessing,
